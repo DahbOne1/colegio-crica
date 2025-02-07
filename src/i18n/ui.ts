46afd2578d65df74175ee3d6b5e40bd5958fb096
David Hernandez <David.arcos1000@gmail.com>
2025-02-07 12:25:48 -0600

    arreglos en preescolar

---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+    en: 'English',
+    fr: 'Français',
+    es: 'Español',
+}
+
+export const defaultLanguage = 'es';
+
+export const ui = {
+    en: {
+        title: 'Hello World',
+        subtitle: 'Welcome to the world of internationalization',
+    },
+    fr: {
+        title: 'Bonjour le monde',
+        subtitle: 'Bienvenue dans le monde de l\'internationalisation',
+    },
+    es: {
+        title: 'Hola Mundo',
+        subtitle: 'Bienvenido al mundo de la internacionalización',
+    },
+} as const
